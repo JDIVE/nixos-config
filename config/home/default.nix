809d0ac6e8f5659e9e9097acac4167ffc1d822ad
JDIVE <26393518+JDIVE@users.noreply.github.com>
2024-04-26 22:52:56 +0100

    Initial Commit

---
+++ config/home/default.nix
@@ -0,0 +1,30 @@
+{ pkgs, config, ... }:
+
+{
+  imports = [
+    # Enable &/ Configure Programs
+    ./alacritty.nix
+    ./bash.nix
+    ./gtk-qt.nix
+    ./hyprland.nix
+    ./kdenlive.nix
+    ./kitty.nix
+    ./neofetch.nix
+    ./neovim.nix
+    ./packages.nix
+    ./rofi.nix
+    ./starship.nix
+    ./waybar.nix
+    ./wlogout.nix
+    ./swappy.nix
+    ./swaylock.nix
+    ./swaync.nix
+    ./vscode.nix
+    ./wezterm.nix
+    ./zeroad.nix
+    ./zsh.nix
+
+    # Place Home Files Like Pictures
+    ./files.nix
+  ];
+}
