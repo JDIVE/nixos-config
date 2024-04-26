fc17c7167399e5a04c31c2e48b2e8d1ddd7e0b58
JDIVE <26393518+JDIVE@users.noreply.github.com>
2024-04-26 23:18:29 +0100

    Removed unrequired extensions

--- config/home/vscode.nix
+++ config/home/vscode.nix
@@ -4,9 +4,7 @@
 programs.vscode = {
   enable = true;
   extensions = with pkgs.vscode-extensions; [
-    dracula-theme.theme-dracula
-    vscodevim.vim
-    yzhang.markdown-all-in-one
+   bbenoist.nix
   ];
 };
 }
